cc24b9ff35aeca8395fe1d8016d3f94b6a06066a
TechHog <77560013+TechHog8984@users.noreply.github.com>
2024-08-10 17:05:49 -0400

    Add files via upload

---
+++ update_discord.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -f discord.tar.gz ]; then
+	rm discord.tar.gz
+fi
+if [ -d /usr/share/discord ]; then
+	sudo rm -rf /usr/share/discord
+fi
+if [ -f ~/.local/share/applications/discord.desktop ]; then
+	sudo rm ~/.local/share/applications/discord.desktop
+fi
+
+baseurl=$(curl "https://discord.com/api/download?platform=linux&format=tar.gz" | grep -Po "\"https:\/\/dl\.discordapp\.net\/apps\/linux\/(\d+\.\d+\.\d+)")
+version=${baseurl:38}
+
+newurl="${baseurl:1}/discord-${version}.tar.gz"
+
+curl $newurl -o discord.tar.gz
+
+tar -xf discord.tar.gz
+rm discord.tar.gz
+cp Discord/discord.desktop ~/.local/share/applications/discord.desktop
+sudo mv Discord /usr/share/discord
